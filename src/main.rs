ceb2a0714578e40aea5629f99581074407da9dcf
Thorsten Ehlers <thorsten.ehlers@googlemail.com>
2021-11-28 18:37:59 +0100

    Use a more idiomatic startup setup
    See https://github.com/marcusbuffett/bevy_snake/issues/8

--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,7 @@ fn main() {
     App::build()
         .add_startup_system(setup::setup.system())
         .add_plugins(DefaultPlugins)
-        .add_startup_stage(
-            "game setup",
-            SystemStage::single(spawner::spawn_diplopod.system()),
-        )
+        .add_startup_system_to_stage(StartupStage::PostStartup, spawner::spawn_diplopod.system())
         .insert_resource(WindowDescriptor {
             title: "Diplopod".to_string(),
             width: 400.0,
