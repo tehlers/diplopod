a538e6629ee0662292cdf3040673bbbd53f3b255
Thorsten Ehlers <thorsten.ehlers@googlemail.com>
2022-02-01 18:51:30 +0100

    Support WASD key binding in addition to the arrow keys

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -5,19 +5,19 @@ pub fn player_input(keyboard_input: Res<Input<KeyCode>>, mut heads: Query<&mut D
     if let Some(mut head) = heads.iter_mut().next() {
         let mut direction = Vec2::ZERO;
 
-        if keyboard_input.pressed(KeyCode::Left) {
+        if keyboard_input.any_pressed([KeyCode::Left, KeyCode::A]) {
             direction = Vec2::new(-1.0, 0.0);
         }
 
-        if keyboard_input.pressed(KeyCode::Right) {
+        if keyboard_input.any_pressed([KeyCode::Right, KeyCode::D]) {
             direction = Vec2::new(1.0, 0.0);
         }
 
-        if keyboard_input.pressed(KeyCode::Up) {
+        if keyboard_input.any_pressed([KeyCode::Up, KeyCode::W]) {
             direction = Vec2::new(direction.x, 1.0);
         }
 
-        if keyboard_input.pressed(KeyCode::Down) {
+        if keyboard_input.any_pressed([KeyCode::Down, KeyCode::S]) {
             direction = Vec2::new(direction.x, -1.0);
         }
 
