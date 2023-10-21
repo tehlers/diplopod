32a800cc3f92db57ba79d9b3aef68d44d76c0d6d
Thorsten Ehlers <thorsten.ehlers@googlemail.com>
2023-10-21 19:38:26 +0200

    Persist highscore

--- src/highscore.rs
+++ src/highscore.rs
@@ -1,8 +1,13 @@
+use anyhow::anyhow;
+use anyhow::Result;
 use bevy::time::common_conditions::on_timer;
 use bevy::utils::Duration;
 use bevy::{input::keyboard::KeyboardInput, input::ButtonState, prelude::*};
+use directories::ProjectDirs;
+use std::fs::File;
+use std::io::Write;
 
-use crate::game::resources::{Highscore, Lastscore};
+use crate::game::resources::{self, Highscore, Lastscore};
 use crate::prelude::TITLE;
 
 use super::{despawn_screen, GameState};
@@ -15,6 +20,10 @@ const TITLE_COLOR: Color = Color::ANTIQUE_WHITE;
 const HEADLINE_COLOR: Color = Color::GRAY;
 const HIGHSCORE_COLOR: Color = Color::WHITE;
 const INITIAL_DELAY_MILLISECONDS: u64 = 500;
+
+const QUALIFIER: &str = "com.github";
+const ORGANIZATION: &str = "tehlers";
+const HIGHSCORE: &str = "highscore";
 
 #[derive(Component)]
 struct OnHighscoreScreen;
@@ -40,8 +49,66 @@ impl Plugin for HighscorePlugin {
                 OnExit(GameState::Highscore),
                 despawn_screen::<OnHighscoreScreen>,
             )
-            .insert_resource(Lastscore::default())
-            .insert_resource(Highscore::default());
+            .add_systems(
+                Update,
+                save_highscore.run_if(
+                    resource_changed::<Highscore>().and_then(not(resource_added::<Highscore>())),
+                ),
+            )
+            .insert_resource(load_highscore())
+            .init_resource::<Lastscore>();
+    }
+}
+
+/// Load highscore from platform specific data directory (e.g.
+/// `$HOME/.local/share/diplopod/highscore`). If the file is invalid or inaccessible the highscore will
+/// be set back to zero.
+fn load_highscore() -> resources::Highscore {
+    match read_highscore_from_file() {
+        Ok(highscore) => Highscore(highscore),
+        Err(e) => {
+            warn!("{}", e);
+            Highscore::default()
+        }
+    }
+}
+
+/// Reads the highscore from the platform specific data directory and tries to parse the value as
+/// `u16`.
+fn read_highscore_from_file() -> Result<u16> {
+    if let Some(projects_dirs) = ProjectDirs::from(QUALIFIER, ORGANIZATION, TITLE) {
+        Ok(std::fs::read_to_string(projects_dirs.data_dir().join(HIGHSCORE))?.parse()?)
+    } else {
+        Err(anyhow!(
+            "Unable to retrieve path to data directory. Highscore can't be read."
+        ))
+    }
+}
+
+/// Save highscore to platform specific data directory (e.g.
+/// `$HOME/.local/share/diplopod/highscore`). Errors will be logged but otherwise ignored.
+fn save_highscore(highscore: Res<Highscore>) {
+    match write_highscore_to_file(highscore.0) {
+        Err(e) => warn!("{}", e),
+        _ => (),
+    };
+}
+
+/// Writes the highscore to the platform specific data directory. The file and all necessary
+/// directories are created by the function.
+fn write_highscore_to_file(highscore: u16) -> Result<()> {
+    if let Some(projects_dirs) = ProjectDirs::from(QUALIFIER, ORGANIZATION, TITLE) {
+        let data_dir = projects_dirs.data_dir();
+        std::fs::create_dir_all(data_dir)?;
+
+        let mut file = File::create(data_dir.join(HIGHSCORE))?;
+        write!(file, "{}", &highscore)?;
+
+        Ok(())
+    } else {
+        Err(anyhow!(
+            "Unable to retrieve path to data directory. Highscore can't be read."
+        ))
     }
 }
 
