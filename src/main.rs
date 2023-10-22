08af9110c9d6807a9c34cb283555adad9df3fc48
Thorsten Ehlers <thorsten.ehlers@googlemail.com>
2023-10-22 19:57:56 +0200

    Embed asset folder in binary using `bevy_embedded_assets`

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod highscore;
 mod menu;
 
 use bevy::prelude::*;
+use bevy_embedded_assets::EmbeddedAssetPlugin;
 use game::resources::*;
 use game::systems::*;
 use prelude::TITLE;
@@ -43,14 +44,18 @@ pub enum GameState {
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: TITLE.into(),
-                resolution: (1149., 645.).into(),
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: TITLE.into(),
+                        resolution: (1149., 645.).into(),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .add_before::<bevy::asset::AssetPlugin, _>(EmbeddedAssetPlugin),
+        )
         .add_plugins((
             menu::MenuPlugin,
             highscore::HighscorePlugin,
